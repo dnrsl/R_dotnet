21a98071c4bc424678a81064b0fe23b0acfab31e
Muhdani <43533957+dnrsl@users.noreply.github.com>
2025-06-04 14:24:33 +0700

    Initial commit

---
+++ DevHabit/DevHabit.Api/Program.cs
@@ -0,0 +1,56 @@
+using DevHabit.Api.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options
+        .UseNpgsql(
+            builder.Configuration.GetConnectionString("Database"),
+            npgsqlOptions => npgsqlOptions
+                .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+        .UseSnakeCaseNamingConvention());
+
+builder.Services.AddOpenTelemetry()
+.ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+.WithTracing(tracing => tracing
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation())
+.WithMetrics(metrics => metrics
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation()
+    .AddRuntimeInstrumentation())
+.UseOtlpExporter();
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.IncludeScopes = true;
+    options.IncludeFormattedMessage = true;
+});
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+//app.UseAuthorization(); belum digunakan
+
+app.MapControllers();
+
+await app.RunAsync();
