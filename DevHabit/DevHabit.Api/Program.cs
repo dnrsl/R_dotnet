bbcb129198c3832d8c78d10037f9159bb390a132
Muhdani <43533957+dnrsl@users.noreply.github.com>
2025-06-23 10:39:16 +0700

    refactoring code (vid 29)

--- DevHabit/DevHabit.Api/Program.cs
+++ DevHabit/DevHabit.Api/Program.cs
@@ -1,3 +1,4 @@
+using DevHabit.Api;
 using DevHabit.Api.Database;
 using DevHabit.Api.DTOs.Habits;
 using DevHabit.Api.Entities;
@@ -17,65 +18,15 @@ using OpenTelemetry.Trace;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers(options =>
-{
-    options.ReturnHttpNotAcceptable = true;
-})
-.AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver())
-.AddXmlSerializerFormatters();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
-builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-
-builder.Services.AddProblemDetails(options =>
-{
-    options.CustomizeProblemDetails = context =>
-    {
-        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
-    };
-});
-
-builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-
-builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => 
-    options
-        .UseNpgsql(
-            builder.Configuration.GetConnectionString("Database"),
-            npgsqlOptions => npgsqlOptions
-                .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
-        .UseSnakeCaseNamingConvention());
-
-builder.Services.AddOpenTelemetry()
-.ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
-.WithTracing(tracing => tracing
-    .AddHttpClientInstrumentation()
-    .AddAspNetCoreInstrumentation()
-    .AddNpgsql())
-.WithMetrics(metrics => metrics
-    .AddHttpClientInstrumentation()
-    .AddAspNetCoreInstrumentation()
-    .AddRuntimeInstrumentation())
-.UseOtlpExporter();
-
-builder.Logging.AddOpenTelemetry(options =>
-{
-    options.IncludeScopes = true;
-    options.IncludeFormattedMessage = true;
-});
-
-builder.Services.AddTransient<SortMappingProvider>();
-builder.Services.AddSingleton<ISortMappingDefinition, SortMappingDefinition<HabitDto, Habit>>(_ => HabitMappings.SortMapping);
-
-builder.Services.AddTransient<DataShapingService>();
+builder
+    .AddControllers()
+    .AddErrorHandling()
+    .AddDatabase()
+    .AddObservability()
+    .AddApplicationServices();
 
 WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
